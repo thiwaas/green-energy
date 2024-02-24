5221724265b3f08774cecadcb2f63925d0931bfe
Thiwanka Malinga <thiwankamalinga0729@gmail.com>
2024-02-24 23:45:51 +0530

    feat: initial commit

--- frontend/src/views/seller/Pagination.jsx
+++ frontend/src/views/seller/Pagination.jsx
@@ -17,10 +17,10 @@ const Pagination = ({pageNumber,setPageNumber,totalItem,parPage,showItem}) => {
     if (startPage <= 0) {
         startPage = 1
     }
-    const createBtn = ()=>{
+    const createBtn = () => {
         const btns = [] 
 
-        for (let i =  startPage; i < endPage; i++){
+        for (let i =  startPage; i < endPage; i++) {
             btns.push(
                 <li onClick={() => setPageNumber(i)} className= {`${pageNumber === i ? 'bg-indigo-500 shadow-lg shadow-indigo-500/50 text-white' : 'bg-slate-700 hover:bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 hover:text-white text-[#d0d2d6]'} w-[33px] h-[33px] rounded-full flex justify-center items-center cursor-pointer`
                 }>
@@ -31,11 +31,12 @@ const Pagination = ({pageNumber,setPageNumber,totalItem,parPage,showItem}) => {
         return btns
 
     }
+
     return (
         <ul className='flex gap-3'>
             {
-               pageNumber > 1 && <li onClick={() => setPageNumber(pageNumber - 1)}className='w-[33px] h-[33px] rounded-full flex justify-center items-cenetr bg-slate-700 text-[#d0d2d6] cursor-pointer'>
-                   <BsChevronDoubleLeft/>
+               pageNumber > 1 && <li onClick={() => setPageNumber(pageNumber - 1)} className='w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-700 text-[#d0d2d6] cursor-pointer'>
+                   <BsChevronDoubleLeft />
                </li>   
             }
             {
