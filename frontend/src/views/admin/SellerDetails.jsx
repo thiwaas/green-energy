f3aa9183d388fdcc664e5293bd4315f862704681
Thiwanka Malinga <thiwankamalinga0729@gmail.com>
2024-02-28 09:31:48 +0530

    update : sellerdetail

--- frontend/src/views/admin/SellerDetails.jsx
+++ frontend/src/views/admin/SellerDetails.jsx
@@ -22,24 +22,61 @@ const SellerDetail = () => {
                     <span>Thiwanka Malinga</span>
                   </div>
                   <div className='flex gap-2'>
-                    <span>Name : </span>
-                    <span>Thiwanka Malinga</span>
+                    <span>Email : </span>
+                    <span>thiwankamalinga@gmail.com</span>
                   </div>
                   <div className='flex gap-2'>
-                    <span>Name : </span>
-                    <span>Thiwanka Malinga</span>
+                    <span>Role : </span>
+                    <span>Seller</span>
                   </div>
                   <div className='flex gap-2'>
-                    <span>Name : </span>
-                    <span>Thiwanka Malinga</span>
+                    <span>Status : </span>
+                    <span>Pending</span>
                   </div>
                   <div className='flex gap-2'>
-                    <span>Name : </span>
-                    <span>Thiwanka Malinga</span>
+                    <span>Payment Account : </span>
+                    <span>Active</span>
                   </div>
                 </div>
               </div>
             </div>
+            <div className='w-4/12'>
+              <div className='px-0 md:px-5 py-2'>
+                <div className='py-2 text-lg'>
+                  <h2>Address</h2>
+                </div>
+                <div className='flex justify-between text-sm flex-col gap-2 p-4 bg-slate-800 rounded-md'>
+                  <div className='flex gap-2'>
+                    <span>Shop Name : </span>
+                    <span>Tryst</span>
+                  </div>
+                  <div className='flex gap-2'>
+                    <span>Division : </span>
+                    <span>North Western</span>
+                  </div>
+                  <div className='flex gap-2'>
+                    <span>District : </span>
+                    <span>Kurunegala</span>
+                  </div>
+                  <div className='flex gap-2'>
+                    <span>Town : </span>
+                    <span>Maho</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <form>
+              <div className='flex gap-4 py-3'>
+                <select className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#283046] border border-slate-700 rounded-md text-[#d0d2d6]' name="" id="">
+                   <option value="">Select Status</option>
+                   <option value="active">Active</option>
+                   <option value="deactive">Deactive</option>
+                </select>
+                <button className='bg-blue-500 w-full hover:shadow-blue-500 hover:shadow-lg text-white rounded-md px-7 py-2 w-[170px]'>Submit</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
