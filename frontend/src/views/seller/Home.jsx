a86b6961a8bc49a4f57109bdbfd7bfdc007117ac
Thiwanka Malinga <thiwankamalinga0729@gmail.com>
2024-04-16 21:20:03 +0530

    feat 7

--- frontend/src/views/seller/Home.jsx
+++ frontend/src/views/seller/Home.jsx
@@ -1,54 +1,87 @@
-/* eslint-disable no-unused-vars */
 import React from 'react'
-import { BsChevronDoubleLeft, BsChevronDoubleRight } from 'react-icons/bs';
 
-const Pagination = ({pageNumber,setPageNumber,totalItem,parPage,showItem}) => {
-    
-    let totalPage = Math.ceil(totalItem / parPage)
-    let startPage = pageNumber
-
-    let dif = totalPage - pageNumber ;
-
-    if(dif<=showItem){
-        startPage = totalPage - showItem 
-    }
-    let endPage = startPage < 0 ? showItem : showItem + startPage 
-
-    if (startPage <= 0) {
-        startPage = 1
-    }
-    const createBtn = () => {
-        const btns = [] 
-
-        for (let i =  startPage; i < endPage; i++) {
-            btns.push(
-                <li onClick={() => setPageNumber(i)} className= {`${pageNumber === i ? 'bg-indigo-500 shadow-lg shadow-indigo-500/50 text-white' : 'bg-slate-700 hover:bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 hover:text-white text-[#d0d2d6]'} w-[33px] h-[33px] rounded-full flex justify-center items-center cursor-pointer`
-                }>
-                    {i}
-                </li>
-            ) 
-        }
-        return btns
-
-    }
-
-    return (
-        <ul className='flex gap-3'>
-            {
-               pageNumber > 1 && <li onClick={() => setPageNumber(pageNumber - 1)} className='w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-700 text-[#d0d2d6] cursor-pointer'>
-                   <BsChevronDoubleLeft />
-               </li>   
-            }
-            {
-                createBtn ()
-            }
-            {
-               pageNumber < totalPage  && <li onClick={() => setPageNumber(pageNumber + 1)} className='w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-700 text-[#d0d2d6] cursor-pointer'>
-                   <BsChevronDoubleRight />
-               </li>   
-            }
-        </ul>
-    )
+const SellerDetail = () => {
+  return (
+    <div>
+      <div className='px-2 lg:px-7 pt-5'>
+        <div className='w-full p-4 bg-[#283046] rounded-md'>
+          <div className='w-full flex flex-wrap text-[#d0d2d6]'>
+            <div className='w-3/12 flex justify-center items-center py-3'>
+              <div>
+                <img  className='w-full h-[230px]'src="http://localhost:3000/images/admin.jpg " alt="" />
+              </div>
+            </div>
+            <div className='w-4/12'>
+              <div className='px-0 md:px-5 py-2'>
+                <div className='py-2 text-lg'>
+                  <h2>Basic Info</h2>
+                </div>
+                <div className='flex justify-between text-sm flex-col gap-2 p-4 bg-slate-800 rounded-md'>
+                  <div className='flex gap-2'>
+                    <span>Name : </span>
+                    <span>Thiwanka Malinga</span>
+                  </div>
+                  <div className='flex gap-2'>
+                    <span>Email : </span>
+                    <span>thiwankamalinga@gmail.com</span>
+                  </div>
+                  <div className='flex gap-2'>
+                    <span>Role : </span>
+                    <span>Seller</span>
+                  </div>
+                  <div className='flex gap-2'>
+                    <span>Status : </span>
+                    <span>Pending</span>
+                  </div>
+                  <div className='flex gap-2'>
+                    <span>Payment Account : </span>
+                    <span>Active</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='w-4/12'>
+              <div className='px-0 md:px-5 py-2'>
+                <div className='py-2 text-lg'>
+                  <h2>Address</h2>
+                </div>
+                <div className='flex justify-between text-sm flex-col gap-2 p-4 bg-slate-800 rounded-md'>
+                  <div className='flex gap-2'>
+                    <span>Shop Name : </span>
+                    <span>Tryst</span>
+                  </div>
+                  <div className='flex gap-2'>
+                    <span>Division : </span>
+                    <span>North Western</span>
+                  </div>
+                  <div className='flex gap-2'>
+                    <span>District : </span>
+                    <span>Kurunegala</span>
+                  </div>
+                  <div className='flex gap-2'>
+                    <span>Town : </span>
+                    <span>Maho</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <form>
+              <div className='flex gap-4 py-3'>
+                <select className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#283046] border border-slate-700 rounded-md text-[#d0d2d6]' name="" id="">
+                   <option value="">Select Status</option>
+                   <option value="active">Active</option>
+                   <option value="deactive">Deactive</option>
+                </select>
+                <button className='bg-blue-500 w-full hover:shadow-blue-500 hover:shadow-lg text-white rounded-md px-7 py-2 w-[170px]'>Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Pagination
+export default SellerDetail
